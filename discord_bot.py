1a892914e64c6b26e61035b1a951e20985e57e65
Ryan <rrgallop@gmail.com>
2022-11-28 14:46:24 -0500

    add comments are support for local nicknames

--- discord_bot.py
+++ discord_bot.py
@@ -13,14 +13,22 @@ BOT_RUNNING = False
 load_dotenv()
 client = discord.Client(intents=discord.Intents.all())
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
     for guild in client.guilds:
         print(f"I am a member of {guild.name}:{guild.id}")
 
+
 @client.event
 async def on_message(message):
+    """
+    Main driver function. Gets invoked whenever the bot sees a message in its watched channels.
+    Will filter out messages that it deems not revelant.
+    (For now, the bot only operates in channels that are named "fitness")
+    """
+
     #filter out irrelevant messages
     if message.author == client.user:
         return
@@ -48,7 +56,7 @@ async def on_message(message):
             until_date = today + relativedelta(weekday=MO(+2)) - timedelta(seconds=1)
         else:
             until_date = today + relativedelta(weekday=MO(+1)) - timedelta(seconds=1)
-            
+
         print(f"Counting checkings for {universal_discord_name} between dates {last_monday} and {until_date}")
         async for message in message.channel.history(after=last_monday, before=until_date, limit=None):
             if '!checkin' in message.content.lower() and message.created_at >= last_monday and message.author.name == universal_discord_name:
@@ -63,7 +71,7 @@ async def on_message(message):
 
     if message.content == "!tracker":
         today = datetime.now() + timedelta(days=7)
-        workouts = await get_tracker_information(message, today)
+        workouts = await get_tracker_information(client, message, today)
         leaderboard = construct_leaderboard(workouts)
         message_to_channel = "Here's how we're doing so far this week! :thinking:\n\n" + leaderboard + "\nGreat work everyone! :heart:"
         print(message_to_channel)
@@ -71,6 +79,9 @@ async def on_message(message):
 
 
 async def get_local_server_nickname(message, client):
+    """
+    Get local nickname so the bot displays the name that server users are expecting to see.
+    """
     member_id = message.author.id
     guild_id = message.guild.id
     guild = await client.fetch_guild(guild_id)
@@ -81,6 +92,10 @@ async def get_local_server_nickname(message, client):
 
 
 async def weekly_tracker(message):
+    """
+    Invokes the tracker automatically once a week on Monday morning.
+    Will display all the checkins from all the users that were seen in the previous week.
+    """
     today = datetime.now()
     next_monday = est.localize(today + relativedelta(weekday=MO(+2)))
     print("Weekly tracker running!")
@@ -99,22 +114,40 @@ async def weekly_tracker(message):
             await message.channel.send(message_to_channel)
         await asyncio.sleep(3600)
     
-async def get_tracker_information(message, input_date):
+
+async def get_tracker_information(client, message, input_date):
+    """
+    Populates a dictionary called "workouts".
+    Tracks all the checkins we've seen from every user over the past week.
+    In order to display this correctly, we need to go out and get the server nickname for every user we see.
+    Then we map the server nicknames back to the universal Discord names so we can find checkins easily.
+    """
     workouts = {}
+    usernames = {}
     input_date = input_date.replace(hour=0, minute=0, second=0)
     after_date = input_date + relativedelta(weekday=MO(-2))
     until_date = input_date + relativedelta(weekday=MO(+1)) - timedelta(seconds=1)
     print(f"Getting !checkins from between {after_date} and {until_date}")
     async for m in message.channel.history(after=after_date, before=until_date, limit=None):
-        if m.author.name != "FitBoi" and "!checkin" in m.content.lower():
-            if m.author.name in workouts:
-                workouts[m.author.name] += 1
+        # usernames dictionary is used to map the universal discord name to the local server nickname
+        # so we can display things using names that people are expecting to see :)
+        if m.author.name not in usernames:
+            local_server_nickname = await get_local_server_nickname(message, client)
+            usernames[m.author.name] = local_server_nickname
+        if "!checkin" in m.content.lower():
+            if usernames[m.author.name] in workouts:
+                workouts[usernames[m.author.name]] += 1
             else:
-                workouts[m.author.name] = 1
-
+                workouts[usernames[m.author.name]] = 1
+        
     return workouts
 
+
 def construct_leaderboard(workout_dict):
+    """
+    Construct the leaderboard/tracker. This will display at the beginning of every week (Monday morning),
+    but can also be invoked with the !tracker command
+    """
     workout_counter = Counter(workout_dict)
     return_string = ''
     for person in workout_counter.most_common():
@@ -124,6 +157,7 @@ def construct_leaderboard(workout_dict):
 
     return return_string
 
+# run the bot
 with open("bot_token", "r") as file:
     bot_token = file.read()
 
